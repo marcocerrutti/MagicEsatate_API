6b8f3d7e31c52a9f12e506835d24e186e20fafa3
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-11-06 14:52:52 +0100

    implementing async repository

---
+++ MagicEsatate_WebApi/Repository/EstateRepository.cs
@@ -0,0 +1,62 @@
+﻿using MagicEsatate_WebApi.Data;
+using MagicEsatate_WebApi.Models;
+using MagicEsatate_WebApi.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MagicEsatate_WebApi.Repository
+{
+    public class EstateRepository : IEstateRepository
+    {
+
+        private readonly ApplcationDbContext _db;
+        public EstateRepository(ApplcationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task Create(Estate entity)
+        {
+           await _db.Estates.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<Estate> Get(Expression<Func<Estate, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Estate> query = _db.Estates;
+            if (!tracked)
+            {
+                query = query.Where(filter);
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Estate>> GetAll(Expression<Func<Estate, bool>> filter = null)
+        {
+            IQueryable<Estate> query = _db.Estates;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+            
+        }
+
+        public async Task Remove(Estate entity)
+        {
+            _db.Estates.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+       
+    }
+}
---
+++ MagicEsatate_WebApi/Repository/IRepository/IEstateRepository.cs
@@ -0,0 +1,15 @@
+﻿using MagicEsatate_WebApi.Models;
+using System.Linq.Expressions;
+
+namespace MagicEsatate_WebApi.Repository.IRepository
+{
+    public interface IEstateRepository
+    {
+        Task<List<Estate>> GetAll(Expression<Func<Estate, bool>> filter  = null);
+        Task <Estate> Get(Expression<Func<Estate, bool>> filter = null, bool tracked=true);
+        Task Create(Estate entity);
+        Task Remove(Estate entity);
+        Task Save();
+
+    }
+}
