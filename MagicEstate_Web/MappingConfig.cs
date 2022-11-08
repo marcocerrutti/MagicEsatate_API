8dd42da2b512415934ab007787078a98a5b6e149
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-11-08 12:45:10 +0100

    implementing automapper in webproject and Adding API url in appsettings

---
+++ MagicEstate_Web/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MagicEsatate_Web.Models;
+using MagicEsatate_Web.Models.Dto;
+
+namespace MagicEsatate_Web
+{
+    public class MappingConfig: Profile
+    {
+
+        public MappingConfig()
+        {
+           CreateMap<EstateDTO, EstateCreateDTO>().ReverseMap();
+           CreateMap<EstateDTO, EstateUpdateDTO>().ReverseMap();
+
+            CreateMap<EstateNumberDTO, EstateNumberCreateDTO>().ReverseMap();
+            CreateMap<EstateNumberDTO, EstateNumberUpdateDTO>().ReverseMap();
+
+        }
+
+    }
+}
