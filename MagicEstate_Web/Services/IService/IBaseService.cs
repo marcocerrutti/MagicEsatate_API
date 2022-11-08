76e093cfa04f4a96e749bb7d535d926d54b75c26
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-11-08 13:29:40 +0100

    Implementing BaseService for API

---
+++ MagicEstate_Web/Services/IService/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using MagicEsatate_Web.Models;
+using MagicEstate_Web.Models;
+
+namespace MagicEstate_Web.Services.IService
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest aPIRequest);
+    }
+}
