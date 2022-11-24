89780508ba857652c82521b59e424b6d08561b7e
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-11-24 18:02:39 +0100

    Implement user repository - register

--- MagicEsatate_WebApi/Repository/UserRepository.cs
+++ MagicEsatate_WebApi/Repository/UserRepository.cs
@@ -15,7 +15,12 @@ namespace MagicEsatate_WebApi.Repository
 
         public bool IsUniqueUser(string username)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
         }
 
         public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
@@ -23,9 +28,19 @@ namespace MagicEsatate_WebApi.Repository
             throw new NotImplementedException();
         }
 
-        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
         {
-            throw new NotImplementedException();
+            LocalUser user = new()
+            {
+                UserName = registrationRequestDTO.UserName,
+                Password= registrationRequestDTO.Password,
+                Name = registrationRequestDTO.Name,
+                Role = registrationRequestDTO.Role,
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
         }
     }
 }
