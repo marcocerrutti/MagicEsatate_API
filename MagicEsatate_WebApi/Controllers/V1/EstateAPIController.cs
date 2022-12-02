dbca0abb6878adbd3a472ad17c1dc4a531700652
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-12-02 17:15:32 +0100

    Added Pagination to the API Response Header

--- MagicEsatate_WebApi/Controllers/V1/EstateAPIController.cs
+++ MagicEsatate_WebApi/Controllers/V1/EstateAPIController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
+using System.Text.Json;
 
 namespace MagicEsatate_WebApi.Controllers.V1
 {
@@ -55,7 +56,8 @@ namespace MagicEsatate_WebApi.Controllers.V1
                 {
                     estateList = estateList.Where(u => u.Name.ToLower().Contains(search));
                 }
-
+                Pagination pagination = new() { PageNumber = pageNumber, PageSize = pageSize };
+                Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(pagination));
                 _response.Result = _mapper.Map<List<EstateDTO>>(estateList);
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
